1b3d56f0c30f4d1920e3ad5bdf3103b0e0eea35c
Daniel Chapman <scriptor.deo@gmail.com>
2021-12-01 08:41:53 -0500

    Day 1, Puzzle 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,35 +1,40 @@
 //use std::env;
 #![allow(unused_assignments)]
+#![allow(unused_mut)]
+#![allow(unused_variables)]
 use std::fs;
 
 fn main() {
-    let mut current: u32;
+    let mut first: u32;
+    let mut second: u32;
+    let mut third: u32;
+    let mut fourth: u32;
     let mut previous: u32;
+    let mut current: u32;
     let mut total_increased: u16 = 0;
 
     let input =
         fs::read_to_string("input.txt").expect("Something went wrong reading the input file");
-    let mut lines = input.lines();
-    //first run
-    previous = lines
-        .next()
-        .expect("Expected string contents to be present")
-        .parse::<u32>()
-        .unwrap();
-    current = lines
-        .next()
-        .expect("Expected string contents to be present")
-        .parse::<u32>()
-        .unwrap();
+    let mut lines = input.lines().peekable();
+    first = lines.next().unwrap().parse::<u32>().unwrap();
+    second = lines.next().unwrap().parse::<u32>().unwrap();
+    third = lines.next().unwrap().parse::<u32>().unwrap();
+    fourth = lines.next().unwrap().parse::<u32>().unwrap();
+    previous = first + second + third;
+    current = second + third + fourth;
     if current > previous {
         total_increased = total_increased + 1;
     }
-    for line in lines {
+    while lines.peek() != None {
         previous = current;
-        current = line.parse::<u32>().unwrap();
+        first = second;
+        second = third;
+        third = fourth;
+        fourth = lines.next().unwrap().parse::<u32>().unwrap();
+        current = second + third + fourth;
         if current > previous {
             total_increased = total_increased + 1;
         }
     }
-    println!("Total times increased: {}", total_increased)
+    println!("Total increased: {}", total_increased)
 }
