6ab6f01d79814722e97e17c65768e220d38cff68
Daniel Chapman <scriptor.deo@gmail.com>
2021-12-05 16:57:16 -0500

    Day 5, Parts 1 & 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -13,16 +13,16 @@ fn main() {
         let mut point2 = word.next().unwrap().split(",");
         let mut x1 = point1.next().unwrap().parse::<u16>().unwrap();
         let mut y1 = point1.next().unwrap().parse::<u16>().unwrap();
-        let mut x2 = point2.next().unwrap().parse::<u16>().unwrap();
-        let mut y2 = point2.next().unwrap().parse::<u16>().unwrap();
+        let x2 = point2.next().unwrap().parse::<u16>().unwrap();
+        let y2 = point2.next().unwrap().parse::<u16>().unwrap();
         let x_diff: i32 = i32::from(x1) - i32::from(x2);
         let y_diff:i32 = i32::from(y1) - i32::from(y2);
         if x_diff == 0 {
             if y1 > y2 {
                 while y1 >= y2 {
-                    grid[y2 as usize][x1 as usize] += 1;
-                    println!("Marking {},{}",x1,y2);
-                    y2+=1;
+                    grid[y1 as usize][x1 as usize] += 1;
+                    println!("Marking {},{}",x1,y1);
+                    y1-=1;
                 }
             } else if y2 > y1 {
                 while y2 >= y1 {
@@ -35,9 +35,9 @@ fn main() {
         } else if y_diff == 0 {
             if x1 > x2 {
                 while x1 >= x2 {
-                    grid[y1 as usize][x2 as usize] += 1;
-                    println!("Marking {},{}",x2,y1);
-                    x2+=1;
+                    grid[y1 as usize][x1 as usize] += 1;
+                    println!("Marking {},{}",x1,y1);
+                    x1-=1;
                 }
             } else if x2 > x1 {
                 while x2 >= x1 {
@@ -46,28 +46,29 @@ fn main() {
                     x1+=1;
                 } 
             }
+//For Part 1 I just didn't include the below else statement
         } else {
             if x1 > x2 {
-                while x1 > x2 && y2 > y1 {
+                while x1 >= x2 && y2 >= y1 {
                     grid[y1 as usize][x1 as usize] += 1;
                     println!("Marking {},{}",x2,y1);
                     x1-=1;
                     y1+=1;
                 }
-                while x1 > x2 && y1 > y2 {
+                while x1 >= x2 && y1 >= y2 {
                     grid[y1 as usize][x1 as usize] += 1;
                     println!("Marking {},{}",x2,y2);
                     x1-=1;
                     y1-=1; 
                 }
             } else if x2 > x1  {
-                while x2 > x1 && y2 > y1 {
+                while x2 >= x1 && y2 >= y1 {
                     grid[y1 as usize][x1 as usize] += 1;
                     println!("Marking {},{}",x1,y1);
                     x1+=1;
                     y1+=1;
                 }
-                while x2 > x1 && y1 > y2 {
+                while x2 >= x1 && y1 >= y2 {
                     grid[y1 as usize][x1 as usize] += 1;
                     println!("Marking {},{}",x1,y2);
                     x1+=1;
