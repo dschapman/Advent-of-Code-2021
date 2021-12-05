0297fe967bfb35d75de669aab75b89644c92cbfa
Daniel Chapman <scriptor.deo@gmail.com>
2021-12-05 15:39:31 -0500

    December 5, 2021 3:39 PM

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,4 +1,7 @@
 use std::fs;
+
+
+
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let mut grid = vec![vec![0u8; 10]; 10];
@@ -8,49 +11,84 @@ fn main() {
         let mut point1 = word.next().unwrap().split(",");
         word.next();
         let mut point2 = word.next().unwrap().split(",");
-        let mut x1 = point1.next().unwrap().parse::<u8>().unwrap();
-        let mut y1 = point1.next().unwrap().parse::<u8>().unwrap();
-        let mut x2 = point2.next().unwrap().parse::<u8>().unwrap();
-        let mut y2 = point2.next().unwrap().parse::<u8>().unwrap();
-        let x_diff: i16 = i16::from(x1) - i16::from(x2);
-        let y_diff:i16 = i16::from(y1) - i16::from(y2);
+        let mut x1 = point1.next().unwrap().parse::<u16>().unwrap();
+        let mut y1 = point1.next().unwrap().parse::<u16>().unwrap();
+        let mut x2 = point2.next().unwrap().parse::<u16>().unwrap();
+        let mut y2 = point2.next().unwrap().parse::<u16>().unwrap();
+        let x_diff: i32 = i32::from(x1) - i32::from(x2);
+        let y_diff:i32 = i32::from(y1) - i32::from(y2);
         if x_diff == 0 {
             if y1 > y2 {
-                while y1 > y2 {
+                while y1 >= y2 {
                     grid[y2 as usize][x1 as usize] += 1;
+                    println!("Marking {},{}",x1,y2);
                     y2+=1;
                 }
             } else if y2 > y1 {
-                while y2 > y1 {
+                while y2 >= y1 {
                     grid[y1 as usize][x1 as usize] += 1;
+                    println!("Marking {},{}",x1,y1);
                     y1+=1;
                 } 
             }
             
         } else if y_diff == 0 {
             if x1 > x2 {
-                while x1 > x2 {
+                while x1 >= x2 {
                     grid[y1 as usize][x2 as usize] += 1;
+                    println!("Marking {},{}",x2,y1);
                     x2+=1;
                 }
             } else if x2 > x1 {
-                while x2 > x1 {
+                while x2 >= x1 {
                     grid[y1 as usize ][x1 as usize] += 1;
+                    println!("Marking {},{}",x1,y1);
                     x1+=1;
                 } 
             }
+        } else {
+            if x1 > x2 && y2 > y1{
+                while x1 >= x2 && y2 >=y1 {
+                    grid[y1 as usize][x2 as usize] += 1;
+                    println!("Marking {},{}",x2,y1);
+                    x2+=1;
+                    y1+=1;
+                }
+            } else if x1 > x2 && y1 > y2 {
+                while x1 >= x2 && y1 >=y2 {
+                    grid[y2 as usize][x2 as usize] += 1;
+                    println!("Marking {},{}",x2,y2);
+                    x2+=1;
+                    y2+=1;
+                }
+            } else if x2 > x1 && y2 > y1 {
+                while x2 >= x1 && y2 >=y1 {
+                    grid[y1 as usize][x1 as usize] += 1;
+                    println!("Marking {},{}",x1,y1);
+                    x1+=1;
+                    y1+=1;
+                }
+            } else {
+                while x2 >= x1 && y1 >=y2 {
+                    grid[y2 as usize][x1 as usize] += 1;
+                    println!("Marking {},{}",x1,y2);
+                    x1+=1;
+                    y2+=1;
+                } 
+            }
+            
         }
-        println!("{},{}", x1, y1);
-        println!("{},{}", x2, y2);
     }
     let mut count = 0;
     for vec in grid {
         for item in vec {
+            print!(" {} ",item);
             if item > 1 {
                 count +=1
             }
+            
         }
+        println!();
     }
-    
     println!("{}", count)
 }
