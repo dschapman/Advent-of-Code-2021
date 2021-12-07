efdcb6c5c7c5df2a13e33fa4de7d539c81226ed5
Daniel Chapman <scriptor.deo@gmail.com>
2021-12-06 21:26:27 -0800

    Day 6, Part 1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,7 +1,24 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("input.txt").unwrap();
-    let fish = input.split(",");
-    println!("{:?}", fish)
+    let input = fs::read_to_string("input.txt").unwrap().replace("\n", "");
+    let mut school_of_lantern_fish: Vec<u8> = input
+        .split(",")
+        .map(|item| item.parse::<u8>().unwrap())
+        .collect();
+    let mut day = 0;
+    while day < 256 {
+        let mut new_school: Vec<u8> = vec![];
+        for fish in school_of_lantern_fish.iter_mut() {
+            if *fish == 0 {
+                *fish = 6;
+                new_school.push(8);
+            } else {
+                *fish -= 1;
+            }
+        }
+        school_of_lantern_fish.append(&mut new_school);
+        day += 1;
+    }
+    println!("{}", school_of_lantern_fish.len())
 }
