cf7535afab5903f5bf00efcd077d434ded2cde50
Daniel Chapman <scriptor.deo@gmail.com>
2021-12-11 13:28:34 -0800

    December 11, 2021 1:28 PM

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,5 +1,7 @@
+use std::fs::{self, read_to_string};
+
 fn main() {
     let number_of_steps = 100;
 
-    let grid_of_octupuses = vec![10; vec![10; u8]];
+    let grid_of_octupuses = read_to_string("input.txt")
 }
