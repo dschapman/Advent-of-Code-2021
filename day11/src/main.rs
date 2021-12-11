f289f062bdc95d43eb698022de9ff7cdc86f5f88
Daniel Chapman <scriptor.deo@gmail.com>
2021-12-11 13:48:34 -0800

    December 11, 2021 1:48 PM

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,18 +1,20 @@
 use std::fs;
 
-struct OctoGrid(Vec<Vec<32>>)
+#[derive(Debug)]
+struct OctoGrid(Vec<Vec<u32>>);
 
 fn main() {
     let _number_of_steps = 100;
 
-    let input: Vec<Vec<u32>> = fs::read_to_string("input.txt")
-        .unwrap()
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|number| number.to_digit(10).unwrap())
-                .collect()
-        })
-        .collect();
+    let input: OctoGrid = Iterator::collect::<_>(
+        fs::read_to_string("input.txt")
+            .unwrap()
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|number| number.to_digit(10).unwrap())
+                    .collect()
+            }),
+    );
     println!("{:?}", input)
 }
