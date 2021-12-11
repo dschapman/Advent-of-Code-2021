f55b1b5f8467af885b2fc50274411e4663538491
Daniel Chapman <scriptor.deo@gmail.com>
2021-12-11 14:52:34 -0800

    December 11, 2021 2:52 PM

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -30,10 +30,15 @@ impl OctoGrid {
         self.0[i][j] = 0;
         if i > 0 {
             self.0[i - 1][j] += 1;
-            if self.0
+            if self.0[i - 1][j] == 9 {
+                self.flash(i - 1, j)
+            }
         }
         if i < 9 {
             self.0[i + 1][j] += 1;
+            if self.0[i + 1][j] == 9 {
+                self.flash(i + 1, j)
+            }
         }
     }
 }
