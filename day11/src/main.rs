8c34d27e4f2abd1a8849338dd44aa5fc31eafc13
Daniel Chapman <scriptor.deo@gmail.com>
2021-12-11 13:36:04 -0800

    December 11, 2021 1:36 PM

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -3,5 +3,8 @@ use std::fs;
 fn main() {
     let number_of_steps = 100;
 
-    let input = fs::read_to_string("input.txt").unwrap().lines().map(|line| line.);
+    let input = fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|line| line.chars().map(|number| number.parse::<u8>()));
 }
