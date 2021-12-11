eef66d20c548ce2886c731f16ab40a8a35a36eba
Daniel Chapman <scriptor.deo@gmail.com>
2021-12-11 13:33:04 -0800

    December 11, 2021 1:33 PM

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -3,7 +3,5 @@ use std::fs;
 fn main() {
     let number_of_steps = 100;
 
-    let input = fs::read_to_string("input.txt").unwrap().lines();
-
-    line.map
+    let input = fs::read_to_string("input.txt").unwrap().lines().map(|line| line.);
 }
